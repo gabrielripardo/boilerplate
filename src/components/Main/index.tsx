43de5b6eb6ddc753fec8ed35a164bc2855742147
gabrielripardo <gts.senna@gmail.com>
2022-12-17 02:17:20 -0300

    storybook with knobs

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,24 +1,27 @@
 import * as S from './styles'
 
-const Main = () => (
- 
+const Main = ({
+    title = "React Avançado",
+    description = "Next JS, Jest, Storybook, Styled Components, Boas práticas"
+}) => (
+
         <S.Wrapper>
             <S.Logo
                 src="/img/logo.svg"
                 alt="Imagem de um átomo e R avançado escrito ao lado."
             />
             <S.Title>
-                React Avançado
+                {title}
             </S.Title>
             <S.Description>
-                Typescript, React JS, Next JS e Styled Components
+                {description}
             </S.Description>
             <S.Illustration
                 src="/img/hero-illustration.svg"
                 alt="Um desenvolvedor de frente para uma tela com código."
             />
         </S.Wrapper>
-  
+
 )
 
 export default Main
